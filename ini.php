c1cdc08c24a98133ae4ee640b4abf95b45d16cad
sebbie34110 <37373858+sebbie34110@users.noreply.github.com>
2018-03-21 14:14:59 +0100

    Update ini.php

--- ini.php
+++ ini.php
@@ -1,14 +1,20 @@
 <?php
-session_start();
+
 /**
  * --------------------------------------------------
  * DB
  * --------------------------------------------------
 **/
 if( !defined('GENERAL')) {
-  define(GENERAL, 1);
-  const DB_HOST = 'localhost';
-  const DB_NAME = 'galaxseed';
-  const DB_LOGIN = 'root';
-  const DB_PWD = '';
+  define('GENERAL', 1);
+
+  define("DB_HOST", "localhost");
+  define("DB_NAME", "galax_seed");
+  define("DB_LOGIN", "root");
+  define("DB_PWD", "");
+
+//  const DB_HOST = 'localhost';
+//  const DB_NAME = 'galax_seed';
+//  const DB_LOGIN = 'root';
+//  const DB_PWD = '';
 }
