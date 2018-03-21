26e44c52884939bd54120b84d37ab2b0db9951c7
sebbie34110 <37373858+sebbie34110@users.noreply.github.com>
2018-03-21 14:16:21 +0100

    Update galax_inscription.php

--- galax_inscription.php
+++ galax_inscription.php
@@ -46,7 +46,7 @@ if (isset($_POST['identifiant']) && isset($_POST['passe']) && isset($_POST['conf
 
               $req->bindValue ('id', NULL,PDO::PARAM_INT);
               $req->bindValue('identidiant', $identifiant, PDO::PARAM_STR);
-              $req->bindValue('passe', password_hash($passe, PASSWORD_DEFAULT, PDO::PARAM_STR);
+              $req->bindValue('passe', password_hash($passe, PASSWORD_DEFAULT, PDO::PARAM_STR));
               $req->bindValue('mail', $mail, PDO::PARAM_STR);
 
               $req->execute();
@@ -74,29 +74,31 @@ if (isset($_POST['identifiant']) && isset($_POST['passe']) && isset($_POST['conf
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
     <title>Inscription Galaxseed</title>
+
 </head>
 <body>
+  <div class="container">
+    <h1>Page d'incription</h1>
 
-<h1>Page d'incription</h1>
+    <p>Renseigner les champs suivants: </p>
 
-<p>Renseigner les champs suivants: </p>
-
-<div>
-    <form method="post" action="">
-        <label for="identifiant">Votre pseudo : </label><input type="text" name="identifiant" id="identifiant" value=""><br/>
-        <label for="passe">Votre mot de passe : </label><input type="password" name="passe" id="passe" value=""><br/>
-        <label for="confirm">Confirmer votre mot de passe : </label><input type="password" name="confirm" id="confirm" value=""><br/>
-        <label for="mail">Votre mail : </label><input type="text" name="mail" id="mail" value=""><br/>
-        <input type="submit" value="S'inscrire">
-    </form>
-</div>
-<?php if(!empty($msg)){
-    echo '<div class="' . $msgClass . '">' . $msg . '</div>';
-} ?>
+    <div>
+        <form method="post" action="">
+            <label for="identifiant">Votre pseudo : </label><input type="text" name="identifiant" id="identifiant" value=""><br/>
+            <label for="passe">Votre mot de passe : </label><input type="password" name="passe" id="passe" value=""><br/>
+            <label for="confirm">Confirmer votre mot de passe : </label><input type="password" name="confirm" id="confirm" value=""><br/>
+            <label for="mail">Votre mail : </label><input type="text" name="mail" id="mail" value=""><br/>
+            <input type="submit" value="S'inscrire">
+        </form>
+    </div>
+    <?php if(!empty($msg)){
+        echo '<div class="' . $msgClass . '">' . $msg . '</div>';
+    } ?>
 
 
-<a type="button" href="?retour">RETOUR à la page de connexion</a>
-
+    <a type="button" href="?retour">RETOUR à la page de connexion</a>
+  </div>
 </body>
 </html>
