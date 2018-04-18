7403eb0c98e6526e9759151f17a72d0e702b714b
Raphael Vignau <37373230+tyramon@users.noreply.github.com>
2018-04-18 15:09:06 +0200

    Add files via upload

---
+++ class/UserManager.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: webuser1801
+ * Date: 18/04/2018
+ * Time: 14:18
+ */
+
+class UserManager
+{
+    private $_pdo;
+
+    public function connexion(){
+
+    }
+
+    public function newUser(){
+
+    }
+
+    public function delete(){
+
+    }
+
+    public function edit(){
+
+    }
+}
