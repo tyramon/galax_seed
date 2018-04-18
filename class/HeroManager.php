d6362f58f1391e2460f14dc6529889ceb079a778
Raphael Vignau <37373230+tyramon@users.noreply.github.com>
2018-04-18 15:21:40 +0200

    Add files via upload

---
+++ class/HeroManager.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: webuser1801
+ * Date: 18/04/2018
+ * Time: 14:20
+ */
+
+class HeroManager
+{
+    private $_pdo;
+
+    public function initHero(){
+
+    }
+
+    public function killHero(){
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPdo()
+    {
+        return $this->_pdo;
+    }
+
+    /**
+     * @param mixed $pdo
+     */
+    public function setPdo($pdo)
+    {
+        $this->_pdo = $pdo;
+    }
+}
